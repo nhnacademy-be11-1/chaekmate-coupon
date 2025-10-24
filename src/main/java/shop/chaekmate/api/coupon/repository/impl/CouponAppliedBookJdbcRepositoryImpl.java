d5cb33064bbb0aaf7111f5343ef8a8a4b9bcbfd6
jeomxon <jeomxon@gmail.com>
2025-10-24 16:27:52 +0900

    fix: 쿠폰 적용 대상 base entity 상속받도록 변경에 따른 burk insert 코드 수정

--- src/main/java/shop/chaekmate/api/coupon/repository/impl/CouponAppliedBookJdbcRepositoryImpl.java
+++ src/main/java/shop/chaekmate/api/coupon/repository/impl/CouponAppliedBookJdbcRepositoryImpl.java
@@ -1,6 +1,8 @@
 package shop.chaekmate.api.coupon.repository.impl;
 
 import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,12 +22,15 @@ public class CouponAppliedBookJdbcRepositoryImpl implements CouponAppliedBookJdb
     @Override
     public void saveAllInBatch(Collection<CouponAppliedBook> couponAppliedBooks) {
         jdbcTemplate.batchUpdate(
-                "INSERT INTO `coupon_applied_book` (coupon_policy_id, book_id) VALUES (?, ?)",
+                "INSERT INTO `coupon_applied_book` (coupon_policy_id, book_id, created_at, updated_at) VALUES (?, ?, ?, ?)",
                 couponAppliedBooks,
                 batchSize,
                 (PreparedStatement ps, CouponAppliedBook couponAppliedBook) -> {
+                    LocalDateTime now = LocalDateTime.now();
                     ps.setLong(1, couponAppliedBook.getCouponPolicy().getId());
                     ps.setLong(2, couponAppliedBook.getBookId());
+                    ps.setTimestamp(3, Timestamp.valueOf(now));
+                    ps.setTimestamp(4, Timestamp.valueOf(now));
                 }
         );
     }
--- src/main/java/shop/chaekmate/api/coupon/repository/impl/CouponAppliedCategoryJdbcRepositoryImpl.java
+++ src/main/java/shop/chaekmate/api/coupon/repository/impl/CouponAppliedCategoryJdbcRepositoryImpl.java
@@ -1,6 +1,8 @@
 package shop.chaekmate.api.coupon.repository.impl;
 
 import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,12 +22,15 @@ public class CouponAppliedCategoryJdbcRepositoryImpl implements CouponAppliedCat
     @Override
     public void saveAllInBatch(Collection<CouponAppliedCategory> couponAppliedCategories) {
         jdbcTemplate.batchUpdate(
-                "INSERT INTO `coupon_applied_category` (coupon_policy_id, category_id) VALUES (?, ?)",
+                "INSERT INTO `coupon_applied_category` (coupon_policy_id, category_id, created_at, updated_at) VALUES (?, ?, ?, ?)",
                 couponAppliedCategories,
                 batchSize,
                 (PreparedStatement ps, CouponAppliedCategory couponAppliedCategory) -> {
+                    LocalDateTime now = LocalDateTime.now();
                     ps.setLong(1, couponAppliedCategory.getCouponPolicy().getId());
                     ps.setLong(2, couponAppliedCategory.getCategoryId());
+                    ps.setTimestamp(3, Timestamp.valueOf(now));
+                    ps.setTimestamp(4, Timestamp.valueOf(now));
                 }
         );
     }
