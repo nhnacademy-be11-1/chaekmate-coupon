0d86f4b3757d960ede2cee3a3e6efdf55ed952d9
chzhxkd100 <chzhxkd100@naver.com>
2025-11-23 14:18:40 +0900

    fix: 테스트 수정

--- src/test/java/shop/chaekmate/api/coupon/controller/CouponAdminControllerTest.java
+++ src/test/java/shop/chaekmate/api/coupon/controller/CouponAdminControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,7 +33,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@Rollback
 @Transactional
 @AutoConfigureMockMvc
 @SuppressWarnings("NonAsciiCharacters")
